3fea3625e3bed423ed76bfe551b16b066de47254
Ben Meier <ben@meierhost.com>
2024-12-08 22:29:21 +0000

    feat: mvp output with ci fix: commit missing files

--- README.md
+++ README.md
@@ -14,13 +14,10 @@ Let's start with what I want to represent in my prompt:
 
 1. The current working directory with $HOME replaced by ~
 2. The git status if we're in a git directory
-3. The exit code of the previous command
-4. The elapsed time of the previous command
-5. The current user id
-6. The current machine hostname
-7. Some colour or styling if the terminal supports this
+3. The exit code of the previous command (if not 0)
+4. The current username or id
 
-Firstly, lets assume that `PS1` is set by `PROMPT_COMMAND` in some way. Either, by having a bash function to capture the output, or by outputting some `eval`-able expression that sets it. So this can capture (3) and calculate (1), (2), (4) assuming that we have a start time of the previous command. Then it can also inspect the terminal to determine (5). I can use a `PS0` command to capture and store the start time of the command somewhere for the next invocation of `PROMPT_COMMAND`.
+Firstly, lets assume that `PS1` is set by `PROMPT_COMMAND` in some way. Either, by having a bash function to capture the output, or by outputting some `eval`-able expression that sets it. So this can capture (3) and calculate (1), (2), and (4).
 
 This leaves us with the form of `PROMPT_COMMAND='eval $(nuprompt ${?})` (with `${?}` capturing the previous exit code). One thing I did in the previous version of my prompt was to insert a newline if the prompt wasn't at the left hand margin. This could happen if the previous command output _didn't_ have a newline itself, however, after using this for some time I found that it wasn't clever enough and would frequently break commands if I typed them too fast.
 
--- src/main.rs
+++ src/main.rs
@@ -1,95 +1,146 @@
+use std::env::args_os;
 use std::error::Error;
+use std::ffi::OsString;
 use std::io::{stdout, BufWriter, Write};
 use std::os::unix::prelude::OsStrExt;
 use std::path::PathBuf;
 use std::str::FromStr;
+use std::time::Instant;
 use git2::{Repository, Status, StatusOptions};
 use log::debug;
 
+fn main() -> Result<(), Box<dyn Error>> {
+    // we output some debug logs which can be turned on if needed.
+    env_logger::init_from_env(env_logger::Env::default().filter("NUPROMPT_RUST_LOG"));
+    
+    // expect a status code as the first positional arg
+    let exit_code = args_os().nth(1).filter(|a| !a.eq("0"));
+    
+    // cwd comes from libc or from the env var
+    let possible_cwd = std::env::current_dir().ok()
+        .or_else(|| std::env::var_os("PWD").map(PathBuf::from));
+    
+    // try and parse git status if were in a repo
+    let start_time = Instant::now();
+    let (cwd, git_bits): (PathBuf, Option<GitBits>) = match possible_cwd {
+        Some(p) => {
+            debug!("looking for git repo from working directory: {:?}", p);
+            let ceil: &[PathBuf] = &[];
+            match Repository::open_ext(&p, git2::RepositoryOpenFlags::empty(), ceil) {
+                Ok(r) => (shorted_path_buf(p), Some(GitBits::from_repo(&r)?)),
+                Err(e) => {
+                    debug!("could not open repository: {:?}", e);
+                    (shorted_path_buf(p), None)
+                }
+            }
+        },
+        None => (PathBuf::new(), None),
+    };
+    debug!("scanned for git repo in {:?}", start_time.elapsed());
+
+    // the username or uid:guid
+    let username = users::get_current_username()
+        .unwrap_or_else(|| OsString::from(format!("{}:{}", users::get_current_uid(), users::get_current_gid())));
+    debug!("found user: {:?}", username);
+
+    // prepare the buffered writer
+    let mut stdout_lock = stdout().lock();
+    let mut buf_writer = BufWriter::new(&mut stdout_lock);
+    
+    buf_writer.write_all(b"PS1='[")?;
+    if let Some(exit_code) = exit_code {
+        buf_writer.write_all(exit_code.as_bytes())?;
+        buf_writer.write_all(b" ")?;
+    }
+    buf_writer.write_all(username.as_bytes())?;
+    buf_writer.write_all(b" ")?;
+    if let Some(git_bits) = git_bits {
+        write_with_escaped_quote(git_bits.head_ref.as_bytes(), &mut buf_writer)?;
+        git_bits.write_elements(&mut buf_writer)?;
+        buf_writer.write_all(b" ")?;
+    }
+    
+    write_with_escaped_quote(cwd.as_os_str().as_bytes(), &mut buf_writer)?;
+    buf_writer.write_all(b" > '")?;
+    buf_writer.flush()?;
+    Ok(())
+}
+
+/// write some raw bytes but make sure we escape any single quotes.
+fn write_with_escaped_quote(input: &[u8], mut w: impl Write) -> Result<(), std::io::Error> {
+    for (i, x) in input.split(|u| *u == b'\'').enumerate() {
+        if i > 0 {
+            w.write_all(b"'\\''")?;
+        }
+        w.write_all(x)?;
+    }
+    Ok(())
+}
+
+/// GitBits holds the result of scanning the git repo for current status.
 struct GitBits {
+    head_ref: String,
     index_modified: bool,
     worktree_modified: bool,
     untracked_files: bool,
 }
 
-fn main() -> Result<(), Box<dyn Error>> {
-    env_logger::init();
+impl GitBits {
 
-    let username = users::get_current_username();
-    let pwd = std::env::var("PWD").map(PathBuf::from).ok().map(|mut p| {
-        let ceil: &[PathBuf] = &[];
-        let git_bits: Option<(String, GitBits)> = Repository::open_ext(&p, git2::RepositoryOpenFlags::empty(), ceil).ok().map_or_else(|| {
-            None
-        }, |r| {
-            let mut gb = GitBits{
-                index_modified: false,
-                worktree_modified: false,
-                untracked_files: false,
-            };
-            if let Ok(x) = r.statuses(Some(StatusOptions::new().include_ignored(false).include_untracked(true).exclude_submodules(true).include_unreadable(false))) {
-                for x in x.iter() {
-                    debug!("git status {:?}: {:?}", x.path(), x.status());
-                    match x.status() {
-                        Status::WT_NEW => {gb.untracked_files = true}
-                        Status::WT_MODIFIED => {gb.worktree_modified = true}
-                        Status::WT_DELETED => {gb.worktree_modified = true}
-                        Status::WT_TYPECHANGE => {gb.worktree_modified = false}
-                        Status::WT_RENAMED => {gb.worktree_modified = true}
-                        Status::INDEX_NEW => {gb.index_modified = true}
-                        Status::INDEX_MODIFIED => {gb.index_modified = true}
-                        Status::INDEX_DELETED => {gb.index_modified = true}
-                        Status::INDEX_TYPECHANGE => {gb.index_modified = true}
-                        Status::INDEX_RENAMED => {gb.index_modified = true}
-                        _ => {}
-                    }
-                }
+    fn from_repo(r: &Repository) -> Result<GitBits, anyhow::Error> {
+        let short_ref = r.head()?.shorthand().unwrap().to_owned();
+        let mut gb = GitBits{
+            head_ref: short_ref,
+            index_modified: false,
+            worktree_modified: false,
+            untracked_files: false,
+        };
+        let statuses = r.statuses(Some(StatusOptions::new()
+            .include_ignored(false)
+            .include_untracked(true)
+            .exclude_submodules(true)
+            .include_unreadable(false)))?;
+        let wt_modified: Status = Status::WT_MODIFIED | Status::WT_DELETED | Status::WT_TYPECHANGE | Status::WT_RENAMED;
+        let index_modified: Status = Status::INDEX_NEW | Status::INDEX_MODIFIED | Status::INDEX_TYPECHANGE | Status::INDEX_RENAMED | Status::INDEX_DELETED;
+        for x in statuses.iter() {
+            debug!("git status {:?}: {:?}", x.path(), x.status());
+            let st = x.status();
+            if st.intersects(wt_modified) {
+                gb.worktree_modified = true;
             }
-            match r.head() {
-                Ok(h) => Some((h.shorthand().unwrap().to_owned(), gb)),
-                _ => Some(("NO HEAD".to_string(), gb)),
+            if st.intersects(index_modified) {
+                gb.index_modified = true;
             }
-        });
-
-        if let Ok(h) = std::env::var("HOME").map(PathBuf::from) {
-            if p.starts_with(&h) {
-                p = PathBuf::from_str("~").unwrap().join(p.strip_prefix(h).unwrap())
+            if st.contains(Status::WT_NEW) {
+                gb.untracked_files = true;
             }
         }
+        Ok(gb)
+    }
 
-        (p.into_os_string(), git_bits)
-    });
-
-    let mut stdout_lock = stdout().lock();
-    let mut buf_writer = BufWriter::new(&mut stdout_lock);
-    buf_writer.write_all(b"[")?;
-    match username {
-        None => buf_writer.write_all("UNKNOWN USER".as_bytes())?,
-        Some(u) => buf_writer.write_all(u.as_bytes())?,
-    };
-    buf_writer.write_all(b" ")?;
-    match pwd.as_ref() {
-        None => buf_writer.write_all("NO PWD".as_bytes())?,
-        Some(p) => {
-            if let Some(git_bits) = &p.1 {
-                buf_writer.write_all(git_bits.0.as_bytes())?;
-                if git_bits.1.untracked_files || git_bits.1.worktree_modified || git_bits.1.index_modified {
-                    buf_writer.write_all(b":")?;
-                    if git_bits.1.index_modified {
-                        buf_writer.write_all(b"s")?;
-                    }
-                    if git_bits.1.worktree_modified {
-                        buf_writer.write_all(b"d")?;
-                    }
-                    if git_bits.1.untracked_files {
-                        buf_writer.write_all(b"u")?;
-                    }
-                }
-                buf_writer.write_all(b" ")?;
+    fn write_elements(&self, mut w: impl Write) -> Result<(), std::io::Error> {
+        if self.index_modified || self.worktree_modified || self.untracked_files {
+            w.write_all(b":")?;
+            if self.index_modified {
+                w.write_all(b"s")?;
             }
-            buf_writer.write_all(p.0.as_bytes())?;
-        },
-    };
-    buf_writer.write_all(b" > ")?;
-    buf_writer.flush()?;
-    Ok(())
+            if self.worktree_modified {
+                w.write_all(b"d")?;
+            }
+            if self.untracked_files {
+                w.write_all(b"u")?;
+            }
+        }
+        Ok(())
+    }
+
+}
+
+
+/// Replace a prefix of $HOME with ~ in the given path.
+fn shorted_path_buf(input: PathBuf) -> PathBuf {
+    match std::env::var("HOME").map(PathBuf::from) {
+        Ok(h) if input.starts_with(&h) => PathBuf::from_str("~").unwrap().join(input.strip_prefix(h).unwrap()),
+        _ => input,
+    }
 }
